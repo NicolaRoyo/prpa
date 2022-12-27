803a7a182fe299c73fdee155db0c8a6bc12f387f
Nicole Arroyo <nicole.m.arroyo@gmail.com>
2022-12-27 00:54:26 -0600

    Add router setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import OutreachDirectoryPage from './pages/OutreachDirectoryPage';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import OutreachDirectoryPage from './pages/OutreachDirectoryPage';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 import './App.css';
-
-
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <OutreachDirectoryPage />
+      <Routes> 
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<OutreachDirectoryPage />} />
+      </Routes>
       <Footer />  
     </div>
   );
