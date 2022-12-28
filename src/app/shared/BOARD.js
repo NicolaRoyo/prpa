edbba3a69945bb4780c7987e1b9b882ee800f478
Nicole Arroyo <nicole.m.arroyo@gmail.com>
2022-12-28 12:09:18 -0600

    Add BOARD.js

---
+++ src/app/shared/BOARD.js
@@ -0,0 +1,59 @@
+import waldemarPhoto from '../assets/img/board/waldemar-cruz.jpg';
+import marilynPhoto from '../assets/img/board/marilyn-gomez.jpg';
+import carlosPhoto from '../assets/img/board/carlos-rivera.jpg';
+import rayPhoto from '../assets/img/board/ray-calixto.jpg';
+import luisPhoto from '../assets/img/board/luis-arroyo.jpg';
+import tonyPhoto from '../assets/img/board/tony-cortez.jpg';
+import edwinPhoto from '../assets/img/board/edwin-reyes.jpg';
+
+export const BOARD = [
+    {
+        id: 0,
+        name: 'Waldemar Cruz',
+        image: waldemarPhoto ,
+        description:
+            'President'
+    },
+    {
+        id: 1,
+        name: 'Marilyn Gomez',
+        image: marilynPhoto ,
+        description:
+            'Vice President'
+    },
+    {
+        id: 2,
+        name: 'Carlos Rivera',
+        image: carlosPhoto ,
+        description:
+            'Secretary / Former President'
+    },
+    {
+        id: 3,
+        name: 'Ray Calixto',
+        image: rayPhoto ,
+        description:
+            'Sergeant-at-Arms'
+    },
+    {
+        id: 4,
+        name: 'Luis Arroyo',
+        image: luisPhoto ,
+        description:
+            'Treasurer'
+    },
+    {
+        id: 5,
+        name: 'Tony Cortez',
+        image: tonyPhoto ,
+        description:
+            'Board Member'
+    },
+    {
+        id: 6,
+        name: 'Edwin Reyes',
+        image: edwinPhoto ,
+        description:
+            'Board Member / General Counsel'
+    }
+];
