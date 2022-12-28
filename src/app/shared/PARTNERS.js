e48d2ef3a4dda0521fc581bf7fe44f14a00f84a6
Nicole Arroyo <nicole.m.arroyo@gmail.com>
2022-12-28 11:45:05 -0600

    Organize partnership logos and add board member photos

---
+++ src/app/shared/PARTNERS.js
@@ -0,0 +1,120 @@
+import nlleoLogo from '../assets/img/partnerships/partner0NLLEO.png';
+import gallsLogo from '../assets/img/partnerships/partner1GALLS.jpg';
+import pachsLogo from '../assets/img/partnerships/partner2pedro-albizu-campos-hs_orig.png';
+import cclfLogo from '../assets/img/partnerships/partner3cclf.gif';
+import praaLogo from '../assets/img/partnerships/partner4praa-logo_orig.png';
+import outputLogo from '../assets/img/partnerships/partner5OutputLounge.png';
+import prbaLogo from '../assets/img/partnerships/partner6prba-logo.png';
+import omniLogo from '../assets/img/partnerships/partner7omni-health.png';
+import esperanzaLogo from '../assets/img/partnerships/partner8esperanzalogo.jpg';
+import mikeyLogo from '../assets/img/partnerships/partner9mikeyo_orig.jpg';
+import kailaLogo from '../assets/img/partnerships/partner10kaila-designs.jpg';
+import workshopLogo from '../assets/img/partnerships/partner11workshop4200-01.png';
+import ashlandLogo from '../assets/img/partnerships/partner12ashland-flowers-logo.png';
+import paleteriaLogo from '../assets/img/partnerships/partner13paleteria-michoacan.jpg';
+
+export const PARTNERS = [
+    {
+        id: 0,
+        name: 'National Latino Law Enforcement Organization',
+        image: nlleoLogo,
+        description:
+            "The NLLEO is quickly becoming the fastest growing Latino law enforcement organization with members ranging from local, state and federal law enforcement agencies across the country."
+    },
+    {
+        id: 1,
+        name: 'Galls',
+        image: gallsLogo,
+        description:
+            'Galls is your reliable source for quality, in-stock public safety equipment and uniforms. Galls services the needs of law enforcement, military, security, fire, EMS, postal, public transportation and more.'
+
+    },
+    {
+        id: 2,
+        name: 'Dr. Pedro Albizu Campos High School',
+        image: pachsLogo,
+        description:
+            'Founded in 1972, is a member of the Youth Connection Charter School (YCCS) and the Alternative School.'
+
+    },
+    {
+        id: 3,
+        name: 'Chicago Community Loan Fund',
+        image: cclfLogo,
+        description: 
+            'The Chicago Community Loan Fund provides flexible, affordable and responsible financing and technical assistance for community stabilization and development efforts and initiatives that benefit low- to moderate-income neighborhoods, families and individuals throughout metropolitan Chicago.'
+
+    },
+    {
+        id: 4,
+        name: 'Puerto Rican Arts Alliance',
+        image: praaLogo,
+        description: 
+            'We are here to provide you the best cultural experience in the nation through our quality music performances, educational programs, and cutting edge exhibitions.'
+    },
+    {
+        id: 5,
+        name: 'Output Lounge Sports Bar',
+        image: outputLogo,
+        description:
+            'At Output Sports Bar, die-hard Chicago fans will never miss a winning moment with 16 TVs broadcasting every Bulls, Blackhawks, Bears, Cubs and Sox games, plus more.  Come and enjoy two full-service bars, or grab a table and feast on our famous wings and assortment of mouth-watering foods from our menu.'
+    },
+    {
+        id: 6,
+        name: 'Puerto Rican Bar Association',
+        image: prbaLogo,
+        description:
+            'The Puerto Rican Bar Association of Illinois (PRBA) is a not-for-profit entity that was established in 1994 to address issues affecting the Latino lawyers and the Latino community.'
+    },
+    {
+        id: 7,
+        name: 'Omni Healthcare',
+        image: omniLogo,
+        description:
+            'Our mission is to provide comprehensive, quality and culturally sensitive primary and preventive health care services to the medically under-served in Chicago and surrounding communities including the uninsured, under-insured and those with no ability to pay.'
+    },
+    {
+        id: 8,
+        name: 'Esperanza Community Services',
+        image: esperanzaLogo,
+        description:
+            'We provide opportunities for children and adults with intellectual and developmental disabilities to help them reach their fullest potential toward a productive and independent life in the community. Esperanza means hope, expectation, promise, prospect, and confidence.'
+    },
+    {
+        id: 9,
+        name: 'Mikey O Comedy Productions',
+        image: mikeyLogo,
+        description:
+            'Mike Oquendo is a proud Chicagoan with great love for his city and its amazing people. Mike produces Comedy, Theater and Music events in the Chicagoland area and across the country.'
+            
+    },
+    {
+        id: 10,
+        name: 'Kaila Designs, Inc.',
+        image: kailaLogo,
+        description:
+            'We providing attractive and professional marketing pieces for any business large and small.'
+    },
+    {
+        id: 11,
+        name: 'Workshop 4200',
+        image: workshopLogo ,
+        description:
+            "As the first ever CREATORSPACE™, Workshop 4200 is a unique place for artists, artisans, and creators of all stripes. It's a place where connections are made and collaborations are born. As the first test of a big idea, Workshop 4200 is a living social experiment based on the notion that creative small businesses do better when they are supported & encouraged, both by those with resources to help them succeed and their creative peers."
+    },
+    {
+        id: 12,
+        name: 'Ashland Addison Florist',
+        image: ashlandLogo,
+        description:
+            'Established in 1932, Ashland Addison Florist has been a premier provider of beautiful floral arrangements and gorgeous flower bouquets for nearly 100 years.'
+    },
+    {
+        id: 13,
+        name: 'Paletería La Michoacana',
+        image: paleteriaLogo ,
+        description: 
+            'At La Michoacana we produce high-quality and innovative paletas, Coolstix (traditionally known as bolis), and vasitos (cups), spreading felicidad throughout thousands of retail stores in the United States. Our paletas have real ice cream and/or inclusions like fruit, nuts, and chocolate that you can see and taste in every paleta, bolis, or cup.'
+    },
+    
+];
